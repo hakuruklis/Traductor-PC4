2c11bbaa08f51eca07ca07a1000060059be55bdc
Unknown <hakuruklis@gmail.com>
2018-04-19 11:29:27 -0500

    Cambios

--- Pruebas/Transcripcion.py
+++ Pruebas/Transcripcion.py
@@ -1,45 +1,53 @@
 import pprint, json
 from openpyxl import Workbook
 from openpyxl import load_workbook
-
+import copy
 
 
 wb = load_workbook(filename = 'Palabras.xlsx')
 ws=wb.active
+
+
 diccionario=[]
+palabras = {}
+
 z=0
 for z in range(181):
+	palabras['id']=z
+	palabras['palabra']=''
+	palabras['sininimos'] = []
+	palabras['definicion']=[]
 	palabra=ws['A'+str(z+1)].value
 	nueva_palabra=palabra.split(". ")
 	nueva_palabra[0]=nueva_palabra[0].split(" | ")
-	nueva_palabra[1]=nueva_palabra[1].split("; ")
-	x=0
-	for x in range(len(nueva_palabra[0])):
-	    if "/" in nueva_palabra[0][x]:
-	        nueva_palabra[0][x]=nueva_palabra[0][x].split(" / ")
+	nueva_palabra[1]=nueva_palabra[1].split(", ")
 	x=0
 	y=0
-	s=0
-	palabras = {}
-	palabras['id']=z
 	for x in range(len(nueva_palabra)):
-	    if x==0:
-	        for y in range(len(nueva_palabra[x])): 
-	                if y==0:
-	                	palabras['palabra#'] = nueva_palabra[x][y]
-	                else:
-	                	palabras['sininimo#'+ str(s+1)] = nueva_palabra[x][y]
-	                	s+=1
-	    elif x==1:
-	        for y in range(len(nueva_palabra[x])): 
-	                palabras['definicion#'+str(y+1)] = nueva_palabra[x][y]
+		if x == 0: 
+			for y in range(len(nueva_palabra[0])):
+				if y == 0:
+					palabras['palabra']=nueva_palabra[0][0]
+				else:
+					palabras['sininimos'].append(nueva_palabra[0][y])
+				y+=1
+		elif x==1:
+			y=0
+			for y in range(len(nueva_palabra[1])):
+					palabras['definicion'].append(nueva_palabra[1][y])
+	#print(palabras)
+	palabrass = copy.deepcopy(palabras)
 	z+=1
-	diccionario.append(palabras)
+	diccionario.append(palabrass)
+	
 
-#pprint.pprint(diccionario)
+print(len(diccionario))
+print(diccionario)
+#print(palabras)
+
 
 #with open('palabras.json', 'w') as outfile:  
-    json.dump(diccionario, outfile)
+#    json.dump(diccionario, outfile)
 
 
 
--- Pruebas/prueba.py
+++ Pruebas/prueba.py
@@ -4,22 +4,41 @@ from openpyxl import load_workbook
 
 
 
-palabra= "abgayala | abga. hacer a tiempo, enseguida, oportunamente, inmediatamente."
+palabra= "arbag | arbagge | arbaggegwa. a escondidas, a espaldas, a hurtadillas."
 nueva_palabra=palabra.split(". ")
 nueva_palabra[0]=nueva_palabra[0].split(" | ")
-nueva_palabra[1]=nueva_palabra[1].split("; ")
+nueva_palabra[1]=nueva_palabra[1].split(", ")
 x=0
-for x in range(len(nueva_palabra[0])):
-    if "/" in nueva_palabra[0][x]:
-        nueva_palabra[0][x]=nueva_palabra[0][x].split(" / ")
-print(len(nueva_palabra[0]))
+
+print(nueva_palabra[0][0])
 x=0
 y=0
 s=0
 z=0
 palabras = {}
 palabras['id']=z
+palabras['palabra']=''
 palabras['sininimos'] = []
+palabras['definicion']=[]
+
+for x in range(len(nueva_palabra)):
+	if x == 0: 
+		for y in range(len(nueva_palabra[0])):
+			if y == 0:
+				palabras['palabra']=nueva_palabra[0][0]
+			else:
+				palabras['sininimos'].append(nueva_palabra[0][y])
+			y+=1
+	elif x==1:
+		y=0
+		for y in range(len(nueva_palabra[1])):
+				print(nueva_palabra[1][y])
+				palabras['definicion'].append(nueva_palabra[1][y])
+
+
+
+print(palabras)
+
 #for x in range(len(nueva_palabra)):
 #    if x==0:
 #        if len(nueva_palabra[x])              	
